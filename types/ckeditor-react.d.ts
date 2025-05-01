ca5b2b0f9078cd99afbce344f9108d72bdbd960b
vamshi <vamshireddy19421@gmail.com>
2025-05-01 22:59:54 +0530

    completed7

--- types/ckeditor-react.d.ts
+++ types/ckeditor-react.d.ts
@@ -1,12 +1,38 @@
-// types/ckeditor-react.d.ts
-declare module '@ckeditor/ckeditor5-react' {
-    import { ClassicEditor } from '@ckeditor/ckeditor5-build-classic';
-    import { ComponentType } from 'react';
-  
-    export const CKEditor: ComponentType<{
-      editor: typeof ClassicEditor;
-      data: string;
-      onChange: (event: any, editor: any) => void;
-      config?: any;
-    }>;
+declare module '@ckeditor/ckeditor5-build-classic' {
+  interface EditorConfig {
+    [key: string]: any; // Flexible config for toolbar, plugins, etc.
   }
+
+  class ClassicEditor {
+    static builtinPlugins: any[];
+    static defaultConfig: EditorConfig;
+    constructor(element: HTMLElement | string, config?: EditorConfig);
+    destroy(): Promise<void>;
+    getData(): string;
+    setData(data: string): void;
+    editing: {
+      view: {
+        document: {
+          getRoot(): { getDomRoot(): HTMLElement } | null;
+        };
+      };
+    };
+  }
+
+  export default ClassicEditor;
+}
+
+declare module '@ckeditor/ckeditor5-react' {
+  import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+  import { ComponentType } from 'react';
+
+  export const CKEditor: ComponentType<{
+    editor: typeof ClassicEditor;
+    data: string;
+    onReady?: (editor: ClassicEditor) => void;
+    onChange?: (event: Event, editor: ClassicEditor) => void;
+    onError?: (event: Event, error: Error) => void;
+    config?: any;
+    [key: string]: any; // Allow additional props
+  }>;
+}
